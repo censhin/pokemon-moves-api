285adb1a0990756f9ae2e42a4ee5bbb61577cccf
censhin <censhin@gmail.com>
2016-05-24 00:48:59 -0500

    feat(daos): add dao for getting a move by name

--- daos.go
+++ daos.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 	"log"
 )
 
@@ -20,7 +21,7 @@ type Move struct {
 	Tutor bool
 }
 
-func MovesDao() ([]Move){
+func MovesDao() ([]Move) {
 	session, err := mgo.Dial("localhost:27017")
 	if err != nil {
 		panic(err)
@@ -32,6 +33,24 @@ func MovesDao() ([]Move){
 	result := []Move{}
 	iter := collection.Find(nil).Iter()
 	err = iter.All(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return result
+}
+
+func MoveDao(name string) (Move) {
+	session, err := mgo.Dial("localhost:27017")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	collection := session.DB("pokedex").C("moves")
+
+	result := Move{}
+	err = collection.Find(bson.M{"name": name}).One(&result)
 	if err != nil {
 		log.Fatal(err)
 	}
--- server.go
+++ server.go
@@ -2,9 +2,11 @@ package main
 
 import (
     "net/http"
+    "log"
 )
 
 func main() {
     InitRoutes()
     http.ListenAndServe(":8001", nil)
+    log.Println("Starting server on port 8001.")
 }
